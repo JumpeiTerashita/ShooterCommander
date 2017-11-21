c4c7a75948471832a40443d98ce0ec90a7d7e40d
sho akagami <vant.akagami@gmail.com>
2017-11-21 19:57:12 +0900

    テスト用　オープニングシーン、ポップアップスクリプト、ポップアッププレハブ　作成

---
+++ Assets/Akagami/Scripts/Popup.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Popup : MonoBehaviour {
+    [SerializeField]
+    Text buttonText1;
+    [SerializeField]
+    Text buttonText2;
+    [SerializeField]
+    Text buttonText3;
+
+    [SerializeField]
+    Text bodyText;
+
+    Action click1;
+    Action click2;
+    Action click3;
+
+    //public static void Open2ButtonPop(string _body, string _buttonStr1, string _buttonStr2, Action click1, Action click2)
+    //{
+    //    // GameObject popupPrefab = Resources.Load<GameObject>("PopupCanvas");
+    //    // Instantiate<GameObject>(popupPrefab);
+    //    GameObject popupObj = Instantiate<GameObject>(Resources.Load<GameObject>("PopupCanvas"));
+    //    Popup popup = popupObj.GetComponent<Popup>();
+   
+    //    popup.bodyText.text = _body;
+    //    popup.buttonText1.text = _buttonStr1;
+    //    popup.buttonText2.text = _buttonStr2;
+    //    popup.click1 = click1;
+    //    popup.click2 = click2;
+    //}
+
+    public static void Open3ButtonPop(string _body,string _buttonStr1,string _buttonStr2,string _buttonStr3,Action click1,Action click2,Action click3)
+    {
+        GameObject popupObj = Instantiate<GameObject>(Resources.Load<GameObject>("PopupCanvas"));
+        Popup popup = popupObj.GetComponent<Popup>();
+
+        popup.bodyText.text = _body;
+        popup.buttonText1.text = _buttonStr1;
+        popup.buttonText2.text = _buttonStr2;
+        popup.buttonText3.text = _buttonStr3;
+        popup.click1 = click1;
+        popup.click2 = click2;
+        popup.click3 = click3;
+    }
+    public void Onclick1()
+    {
+        click1();
+        Destroy(gameObject);
+    }
+
+    public void Onclick2()
+    {
+        click2();
+        Destroy(gameObject);
+    }
+    public void Onclick3()
+    {
+        click3();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Akagami/Scripts/Scene_Opening.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene_Opening : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Popup.Open2ButtonPop("test", "test", "test", () => Debug.Log(1), () => Debug.Log(2));
+
+        Popup.Open3ButtonPop("test", "test", "test", "test", () => Debug.Log(1), () => Debug.Log(2), () => Debug.Log(3));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
