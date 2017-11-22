c227a68089a00e8f4a8b4e12f9380cb64638667f
Jumpei Terashita <genuineflat@gmail.com>
2017-11-22 15:57:32 +0900

    シューティングシーン　テスト
    コントローラー入力(XInput)を受け取って移動出来るように

---
+++ Assets/Terashita/Scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + speed*Input.GetAxisRaw("Horizontal"),0,transform.position.z + speed*Input.GetAxisRaw("Vertical"));
+    }
+}
