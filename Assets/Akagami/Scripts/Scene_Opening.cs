8f65efd8166f2942fba2f88b83d69099b8de65d3
sho akagami <vant.akagami@gmail.com>
2017-11-24 19:37:16 +0900

    オープニングからTerashita/Scenes/Shootingシーンへの遷移を追加

--- Assets/Akagami/Scripts/Scene_Opening.cs
+++ Assets/Akagami/Scripts/Scene_Opening.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Scene_Opening : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
         //Popup.Open2ButtonPop("test", "test", "test", () => Debug.Log(1), () => Debug.Log(2));
 
-        Popup.Open3ButtonPop("test", "test", "test", "test", () => Debug.Log(1), () => Debug.Log(2), () => Debug.Log(3));
+        Popup.Open3ButtonPop("シューターコマンダー", "テスト", "終了", "おわる",
+            () => SceneManager.LoadScene("Terashita/Scenes/Shooting"), 
+            () => Application.Quit(), 
+            () => Application.Quit());
         
 	}
 	
