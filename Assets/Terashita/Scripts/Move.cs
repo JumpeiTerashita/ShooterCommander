c11cc61d4149d214a80ddbe40241efc649822957
Jumpei Terashita <genuineflat@gmail.com>
2017-12-06 17:15:53 +0900

    飛行機の挙動変更

--- Assets/Terashita/Scripts/Move.cs
+++ Assets/Terashita/Scripts/Move.cs
@@ -10,17 +10,23 @@ namespace KTB
     public class Move : MonoBehaviour
     {
         [SerializeField]
-        float speed = 0.005f;
+        float speed = 0.025f;
+        [SerializeField]
+        float YawSpeed = 3.0f;
 
-        float LeftStick_Horizontal,LeftStick_Vertical,RightStick_Vertical;
+        float LeftStick_Horizontal,LeftStick_Vertical, RightStick_Horizontal,RightStick_Vertical;
 
         // Update is called once per frame
         void Update()
         {
             LeftStick_Horizontal = Input.GetAxisRaw("Horizontal");
             LeftStick_Vertical = Input.GetAxisRaw("Vertical");
+            RightStick_Horizontal = Input.GetAxisRaw("Horizontal2");
             RightStick_Vertical = Input.GetAxisRaw("Vertical2");
-            transform.position = new Vector3(transform.position.x + speed * LeftStick_Horizontal, transform.position.y + speed * RightStick_Vertical, transform.position.z + speed * LeftStick_Vertical);
+
+            transform.Translate(speed * LeftStick_Horizontal,speed * RightStick_Vertical,speed * LeftStick_Vertical);
+            transform.Rotate(transform.up, YawSpeed*RightStick_Horizontal);
+            //transform.position = new Vector3(transform.position.x + speed * LeftStick_Horizontal, transform.position.y + speed * RightStick_Vertical, transform.position.z + speed * LeftStick_Vertical);
         }
     }
 }
