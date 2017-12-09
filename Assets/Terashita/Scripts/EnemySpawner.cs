f51505df4d5a034bb729d062e353833ec3b636ee
Jumpei Terashita <genuineflat@gmail.com>
2017-12-09 23:40:36 +0900

    敵生成オブジェクト EnemySpawner 追加 , 慣性力時限リセットスクリプト StrikeForceReset 追加

---
+++ Assets/Terashita/Scripts/EnemySpawner.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KTB
+{
+    [RequireComponent(typeof(SphereCollider))]
+
+    /// <summary>
+    /// 敵生成オブジェクト　指定座標
+    /// </summary>
+    public class EnemySpawner : MonoBehaviour
+    {
+        GameObject Player;
+        [SerializeField]
+        GameObject Enemy;
+
+        [SerializeField]
+        float SpawnSpan = 5.0f;
+
+        Vector3 SpawnPoint;
+
+        bool isStarted = false;
+        bool isRunning = false;
+        bool isAlreadySpawn = false;
+
+        // Use this for initialization
+        void Start()
+        {
+            SpawnPoint = transform.position;
+            Player = GameObject.Find("Player");
+            if (!Player) Destroy(gameObject);
+        }
+
+        void Update()
+        {
+            if (!isStarted)
+            {
+                isStarted = true;
+                StartCoroutine(SpawnLoop());
+            }
+        }
+
+        IEnumerator SpawnLoop()
+        {
+            if (isRunning) yield break;
+            isRunning = true;
+            //  Debug.Log("I'm Running");
+            if (isAlreadySpawn)
+            {
+                isAlreadySpawn = false;
+            }
+            else
+            {
+                GameObject SpawnedEnemy = Instantiate(Enemy, SpawnPoint, Quaternion.identity);
+                SpawnedEnemy.transform.LookAt(Player.transform.position);
+                Debug.Log(Player.transform.position);
+                Debug.Log(SpawnedEnemy.transform.forward);
+            }
+
+            yield return new WaitForSeconds(SpawnSpan);
+
+
+            isRunning = false;
+            StartCoroutine(SpawnLoop());
+        }
+
+        void OnTriggerStay(Collider col)
+        {
+            if (col.tag == "Enemy") isAlreadySpawn = true;
+        }
+
+        public void SetSpawnSpan(float _sec)
+        {
+            SpawnSpan = _sec;
+        }
+
+    }
+}
---
+++ Assets/Terashita/Scripts/StrikeForceReset.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KTB
+{
+    [RequireComponent(typeof(Rigidbody))]
+    /// <summary>
+    /// 自機なりなんなりが何かとぶつかったとき
+    /// 慣性力が働き続けるので　解消する
+    /// </summary>
+    public class StrikeForceReset : MonoBehaviour
+    {
+        [SerializeField]
+        float ResetSec = 1.0f;
+        // Use this for initialization
+        void Start()
+        {
+            StartCoroutine(Reset());
+        }
+
+        IEnumerator Reset()
+        {
+            yield return new WaitForSeconds(ResetSec);
+            Debug.Log(gameObject.GetComponent<Rigidbody>().velocity);
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            StartCoroutine(Reset());
+        }
+    }
+}
