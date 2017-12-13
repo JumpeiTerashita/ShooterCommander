37b7dc72f6f7671a0ba43722b2a76c4ad5bfc558
Jumpei Terashita <genuineflat@gmail.com>
2017-12-13 15:47:27 +0900

    DestinationHolder + EnemyBehavior 変更　目的地セットしてそっちへ飛べる

--- Assets/Terashita/Scripts/EnemyBehavior.cs
+++ Assets/Terashita/Scripts/EnemyBehavior.cs
@@ -9,17 +9,25 @@ namespace KTB
     /// </summary>
     public class EnemyBehavior : MonoBehaviour
     {
-
+        [SerializeField]
+        float Speed = 0.01f;
+        Vector3 Vec;
         // Use this for initialization
         void Start()
         {
-
+           
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            // 設定した目的地にだんだん向く
+            Vector3 TargetPos = GetComponent<DestinationHolder>().GetDestination();
+            Quaternion targetRotation = Quaternion.LookRotation(TargetPos - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation,targetRotation,Time.deltaTime);
+            
+            // 向いている方向に飛ぶ
+            transform.Translate(new Vector3(0, 0, Speed));
         }
 
         /// <summary>
