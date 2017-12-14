6e2282edc8165e5cad5b856da2a0106cd0dac046
sho akagami <vant.akagami@gmail.com>
2017-12-14 20:21:45 +0900

    AudioPlayer作成、現在の音の値を-1~1で取得

---
+++ Assets/Akagami/Scripts/AudioDataOutPutter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class WaveOutputter : MonoBehaviour
+{
+    private AudioSource audio;
+    float[] waveData_ = new float[1024];
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        audio.GetOutputData(waveData_, 1);
+    }
+}
---
+++ Assets/Akagami/Scripts/AudioPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+    [SerializeField]
+    public AudioClip audio;
+
+    float[] waveData_ = new float[1024];
+
+
+    private AudioSource source;
+
+    private void Start()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+        source.clip = audio;
+        source.Play();
+        source.loop = true;
+        
+    }
+
+    private void Update()
+    {
+
+        source.GetOutputData(waveData_, 1);
+       
+        //Debug.Log(waveData_[0]);
+    }
+
+}
