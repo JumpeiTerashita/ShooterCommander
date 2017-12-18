11138d6b387720453c786b0f10f33e66b288db38
Jumpei Terashita <genuineflat@gmail.com>
2017-12-18 23:05:41 +0900

    軌跡カーテン　上下切り替えボタン(Yボタン)実装,アクセルをギアチェンジに変更などなど

--- Assets/Terashita/Scripts/PillarCreator.cs
+++ Assets/Terashita/Scripts/PillarCreator.cs
@@ -10,10 +10,17 @@ namespace KTB
         float EveryPillarLifeLimit = 1.0f;
 
         [SerializeField]
+        float PillarTopLimit = 5;
+
+        [SerializeField]
         float PillarUnderLimit = -5;
+
+        private float PillarLimit;
 
         [SerializeField]
         GameObject Pillar;
+
+        private bool IsSideUnder;
 
         private GameObject Player;
 
@@ -25,17 +32,32 @@ namespace KTB
         // Use this for initialization
         void Start()
         {
-
+            PillarLimit = PillarUnderLimit;
+            IsSideUnder = true;
         }
 
 
         // Update is called once per frame
         void Update()
         {
+            if (Input.GetButtonDown("Attack_SideChange"))
+            {
+                if (IsSideUnder)
+                {
+                    IsSideUnder = false;
+                    PillarLimit = PillarTopLimit;
+                }
+                else
+                {
+                    IsSideUnder = true;
+                    PillarLimit = PillarUnderLimit;
+                }
+            }
+
             Vector3 PlayerPos = Player.transform.position;
             GameObject InstPillar = Instantiate(Pillar);
-            InstPillar.transform.localScale = new Vector3(InstPillar.transform.localScale.x,PlayerPos.y - PillarUnderLimit, InstPillar.transform.localScale.z);
-            InstPillar.transform.position = new Vector3(PlayerPos.x, PlayerPos.y - (PlayerPos.y - PillarUnderLimit)/2,PlayerPos.z);
+            InstPillar.transform.localScale = new Vector3(InstPillar.transform.localScale.x,PlayerPos.y - PillarLimit, InstPillar.transform.localScale.z);
+            InstPillar.transform.position = new Vector3(PlayerPos.x, PlayerPos.y - (PlayerPos.y - PillarLimit) /2,PlayerPos.z);
             InstPillar.GetComponent<AutoDestroy>().SetDestroyLimit(EveryPillarLifeLimit);
         }
     }
